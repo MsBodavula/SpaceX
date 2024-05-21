154c5b76d036b228ca4f322594b1a26b6c7732b0
MsBodavula <112110432+MsBodavula@users.noreply.github.com>
2024-05-21 10:38:31 -0700

    SpaceZ Controller

---
+++ SpaceXYZ/SpaceZ/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SpaceZ
+{
+    // Interaction logic for App.xaml
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+     
+          
+
+           // MessageBox.Show("Arg =" + e.Args[0].ToString());
+            string SpacecraftName = e.Args[0].ToString();
+            string PayloadName = e.Args[1].ToString();
+            string SpacecraftOrbit = e.Args[2].ToString();
+            string PayloadType = e.Args[3].ToString();
+            MainWindow window = new MainWindow(SpacecraftName, PayloadName, SpacecraftOrbit, PayloadType);
+            this.MainWindow = window;
+
+
+
+            window.Show();
+
+        }
+    }
+}
---
+++ SpaceXYZ/SpaceZ/SpacecraftCallBack.cs
@@ -0,0 +1,41 @@
+﻿using MessageInterface;
+using System;
+using System.ServiceModel;
+using System.Windows;
+
+namespace SpaceZ
+{
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class SpacecraftCallBack : IClient
+    {
+        public void GetMessage(string message, string spacecraftname)
+        {
+            ((MainWindow)Application.Current.MainWindow).TakeMessage(message, spacecraftname);
+        }
+
+
+        public void GetUpdate(int value, string spacecraftname)
+        {
+            switch (value)
+            {
+                case 0:
+                    {
+                        ((MainWindow)Application.Current.MainWindow).AddSpacecraftToList(spacecraftname);
+
+                        break;
+                    }
+
+                case 1:
+                    {
+                        ((MainWindow)Application.Current.MainWindow).RemoveSpacecraftFromList(spacecraftname);
+                        break;
+                    }
+            }
+        }
+
+        public void PlaceHolder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
